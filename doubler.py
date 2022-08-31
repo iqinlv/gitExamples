0ec996bb3dec75b455b07430c09e73914ac5e525
NetAcad DEVASC <changeme@example.com>
2022-08-31 23:56:24 +0000

    first commit

---
+++ doubler.py
@@ -0,0 +1,7 @@
+def doubler(number):
+    result = number * 2
+    return result
+get_number = float(input("Type a number: "))
+Print("Geting your answer")
+answer = doubler(et_number)
+print(answer)
