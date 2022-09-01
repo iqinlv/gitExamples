764d8ab1cbe42ff7afab53e450f9d5cd7cbe98b6
NetAcad DEVASC <changeme@example.com>
2022-09-01 00:12:27 +0000

    added additional print statement

--- doubler.py
+++ doubler.py
@@ -2,6 +2,6 @@ def doubler(number):
     result = number * 2
     return result
 get_number = float(input("Type a number: "))
-Print("Geting your answer")
-answer = doubler(et_number)
+print("Geting your answer")
+answer = doubler(get_number)
 print(answer)
